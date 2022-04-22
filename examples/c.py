144ab2d9ec6f407cf2ba3968c1ef7244dc3c16b4
Jules Labrecque <JULAB130@ulaval.ca>
2022-04-21 20:44:47 -0400

    ajout c)

---
+++ examples/c.py
@@ -0,0 +1,26 @@
+import env_examples  # Modifies path, DO NOT REMOVE
+
+from src import Circuit, Current, Wire, World
+
+
+if __name__ == "__main__":
+    world = World(shape=(81, 81))
+
+    world = World(shape=(73, 73))
+
+    wires = [
+        Wire(start=(11, 11), stop=(11, 62), current=Current(x=0, y=1), voltage=4.5),
+        Wire(start=(11, 62), stop=(56, 62), current=Current(x=1, y=0), voltage=4.5),
+        Wire(start=(56, 62), stop=(62, 62), current=Current(x=1, y=0), voltage=-4.5),
+        Wire(start=(62, 62), stop=(62, 11), current=Current(x=0, y=-1), voltage=-4.5),
+        Wire(start=(62, 11), stop=(17, 11), current=Current(x=-1, y=0), voltage=-4.5),
+        Wire(start=(17, 11), stop=(11, 11), current=Current(x=-1, y=0), voltage=4.5),
+    ]
+
+    circuit = Circuit(wires=wires)
+
+    world.place(circuit)
+
+    world.compute()
+
+    world.show_all()
