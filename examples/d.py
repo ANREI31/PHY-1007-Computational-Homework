8ecead6ff0567d9066be6c3112a23f417a801f25
ANREI31 <anrei31@ulaval.ca>
2022-04-22 23:01:34 -0400

    merge

--- examples/d.py
+++ examples/d.py
@@ -77,3 +77,41 @@ if __name__ == "__main__":
     world.compute()
 
     world.show_all()
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    def get_world_t(t, abs_u=4.5):
+        world = World(shape=(73, 73))
+
+        v = abs_u*np.cos(t)
+        i = abs_u*np.sin(t)
+
+        wires = [
+            Wire(start=(11, 11), stop=(11, 62), current=Current(x=0, y=i), voltage=v),
+            Wire(start=(11, 62), stop=(36, 62), current=Current(x=i, y=0), voltage=v),
+            Wire(start=(36, 62), stop=(62, 62), current=Current(x=i, y=0), voltage=-v),
+            Wire(start=(62, 62), stop=(62, 11), current=Current(x=0, y=-i), voltage=-v),
+            Wire(start=(62, 11), stop=(36, 11), current=Current(x=-i, y=0), voltage=-v),
+            Wire(start=(36, 11), stop=(11, 11), current=Current(x=-i, y=0), voltage=v),
+        ]
+
+        circuit = Circuit(wires=wires)
+
+        world.place(circuit)
+
+        world.compute()
+
+        return world
+    
+    for i in np.linspace(0, 2*np.pi, 20):
+        w = get_world_t(i)
+        w.show_wires_voltage(clim=(-4.5, 4.5), filename='img/voltage/'+f"{i}.png")
+        w.show_potential(clim=(-4.5, 4.5), filename='img/potential/'+f"{i}.png")
+        w.show_electric_field(clim=(0, 3.1), filename='img/electric/'+f"{i}.png")
+        w.show_magnetic_field(clim=(-4e-7, 4e-7), filename='img/magnetic/'+f"{i}.png")
+        w.show_energy_flux(clim=(0, 4), filename='img/energy/'+f"{i}.png")
+"""
